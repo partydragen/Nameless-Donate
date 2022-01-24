24d39eeb3bfc1ecf4765b6c1dca376a754c75a3b
Justman10000 <49990629+Justman10000@users.noreply.github.com>
2022-01-24 12:35:46 +0100

    Added German

---
+++ upload/modules/Donate/language/German/admin.php
@@ -0,0 +1,48 @@
+<?php 
+/*
+ *	Made by Partydragen
+ *  https://partydragen.com/
+ *
+ *  License: MIT
+ *
+ *  EnglishUK Language for Donate module
+ */
+
+$language = array(
+    'settings' => 'Einstellungen',
+    'payments' => 'Zahlungen',
+    'new_donation' => 'Neue Spende',
+    
+    // Settings
+    'paypal_email' => 'PayPal E-Mail',
+    'link_location' => 'Link-Standort.',
+	'icon' => 'Icon',
+	'icon_example' => 'Beispiel: <i class="fa fa-thumbs-up" aria-hidden="true"></i>',
+    'reward_group' => 'Belohnungsgruppe',
+    'success_content' => 'Erfolgreich Spendennachricht',
+    'settings_updated_successfully' => 'Einstellungen erfolgreich aktualisiert.',
+    
+    // Payments
+    'user' => 'Benutzer',
+    'status' => 'Status',
+    'date' => 'Datum',
+    'view' => 'Ansehen',
+    'fee' => 'Gebühr',
+    'no_payments' => 'Keine Zahlungen gefunden!',
+    'viewing_payment' => 'Zahlung {x} ansehen', // Don't replace {x}
+    'transaction' => 'Transaktions ID',
+    'payment_method' => 'Zahlungsmethode ',
+    'viewing_payments_for_user_x' => 'Zahlungen für den Benutzer {x} ansehen', // Don't replace {x}
+    'no_payments_for_user' => 'Für diesen Benutzer wurden keine Zahlungen gefunden.',
+    
+    // Permissions
+    'staffcp_donate_settings' => 'StaffCP &raquo; Donate &raquo; Einstellungen',
+    'staffcp_donate_payments' => 'StaffCP &raquo; Donate &raquo; Zahlungen',
+    
+	// Update Alert
+	'new_update_available_x' => 'Es existiert ein neues Update für das Modul {x}',
+	'new_urgent_update_available_x' => 'Es existiert ein neues dringendes Update für das Modul {x}. Bitte so schnell wie möglich aktualisieren!',
+	'current_version_x' => 'Aktuelle Modulversion: {x}',
+	'new_version_x' => 'Neue Modulversion: {x}',
+	'view_resource' => 'Ressource ansehen',
+);
